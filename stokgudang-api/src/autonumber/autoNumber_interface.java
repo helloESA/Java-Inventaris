5effa4d8d1d36c1274ede07d9e4d45c75aa9d54d
helloESA <m.esa.resmaya@gmail.com>
2020-03-05 20:09:21 +0700

    memperbarui project, laporan masih trouble

---
+++ stokgudang-api/src/autonumber/autoNumber_interface.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package autonumber;
+
+import entity.NumberField;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ *
+ * @author COMPUTER
+ */
+public interface autoNumber_interface extends Remote {
+    String getAutoNumberInt(NumberField field)throws RemoteException;
+}
